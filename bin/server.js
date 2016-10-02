4a5da9ee3d5a7956e995f9acd318721db332ce4e
Valerii Iatsko <dwr@codingbox.io>
2016-10-02 18:18:34 +0200

    Isomorphic part finished

--- bin/server.js
+++ bin/server.js
@@ -1,13 +1,25 @@
-/* eslint no-console: 0 */
+/* eslint no-console: 0, react/jsx-filename-extension: 0 */
 
 import express from 'express';
 import http from 'http';
 import httpProxy from 'http-proxy';
 import path from 'path';
-
+import PrettyError from 'pretty-error';
+import React from 'react';
+import ReactDOM from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import createHistory from 'react-router/lib/createMemoryHistory';
+import { Provider } from 'react-redux';
+import {
+  createStore,
+} from '../src/redux/createStore';
+import getRoutes from '../src/routes';
+import Default from '../src/layouts/Default';
 import { port, apiHost, apiPort } from '../config/env';
 
 const targetUrl = `http://${apiHost}:${apiPort}`;
+const pretty = new PrettyError();
 const app = express();
 const server = new http.Server(app);
 const proxy = httpProxy.createProxyServer({
@@ -19,10 +31,6 @@ app.use('/', express.static(path.resolve(__dirname, '../../public')));
 
 app.use('/api', (req, res) => {
   proxy.web(req, res, { target: `${targetUrl}/api` });
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello from server!');
 });
 
 server.on('upgrade', (req, socket, head) => {
@@ -41,6 +49,41 @@ proxy.on('error', (error, req, res) => {
   const json = { error: 'proxy_error', reason: error.message };
 
   res.end(JSON.stringify(json));
+});
+
+app.use((req, res) => {
+  const memoryHistory = createHistory(req.originalUrl);
+  const store = createStore(memoryHistory);
+  const history = syncHistoryWithStore(memoryHistory, store);
+
+  function hydrateOnClient() {
+    res.send(`<!doctype html>${ReactDOM.renderToString(<Default store={store} />)}`);
+  }
+
+  match({ history, routes: getRoutes(store), location: req.originalUrl },
+  (error, redirectLocation, renderProps) => {
+    if (redirectLocation) {
+      res.redirect(redirectLocation.pathname + redirectLocation.search);
+    } else if (error) {
+      console.error('ROUTER ERROR:', pretty.render(error));
+      res.status(500);
+      hydrateOnClient();
+    } else if (renderProps) {
+      const component = (
+        <Provider store={store} key="provider">
+          <RouterContext {...renderProps} />
+        </Provider>
+      );
+
+      res.status(200);
+
+      global.navigator = { userAgent: req.headers['user-agent'] };
+
+      res.send(`<!doctype html>${ReactDOM.renderToStaticMarkup(<Default component={component} store={store} />)}`);
+    } else {
+      res.status(404).send('Not found');
+    }
+  });
 });
 
 app.listen(port, (err) => {
