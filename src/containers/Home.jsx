30c91088e00c7d7e23b3915280ed274e0448bd14
justboris <boriskan@gmail.com>
2016-12-14 11:46:32 -0500

    npm install and update content

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,6 +4,6 @@ import React from 'react';
 
 export default class Home extends React.Component {
   render() {
-    return <div>Home</div>;
+    return <div>Home for Boris! This is just for testing and studying purpose</div>;
   }
 }
